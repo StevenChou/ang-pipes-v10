602419f0e9c65aaab185f9e06d54d07a770830c4
whalebro <steven.chou.second@gmail.com>
2020-10-13 12:53:28 +0800

    end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes-v10';
+  name: string;
+  date: string;
+  amount: number;
+  height: number;
+  miles: number;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000,
+  };
+
+  onMilesChange(value: string): void {
+    this.miles = parseFloat(value);
+  }
+
+  onHeightChange(value: string): void {
+    this.height = parseFloat(value);
+  }
+
+  onNameChange(value: string): void {
+    this.name = value;
+  }
+
+  onDateChange(value: string): void {
+    this.date = value;
+  }
+
+  onAmountChange(value: string): void {
+    this.amount = parseFloat(value);
+  }
 }
